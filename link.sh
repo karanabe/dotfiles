03dbe456ca39961625f96bd8912dbaf2b91069e1
karanabe <152078880+karanabe@users.noreply.github.com>
2026-05-04 22:12:47 +0900

    repo: update dotfile setup and commit guidance
    Refresh local setup defaults so shell startup and repository
    metadata match the current workstation layout.
    
    Add a tcpdump alias, point Oracle Instant Client at 23_9, drop the
    manual pnpm path, ignore local Codex state, tighten the commit
    template, and remove stale link script placeholders.

--- link.sh
+++ link.sh
@@ -11,10 +11,7 @@ ln -s $FILE_DIR/.common_alias $HOME/.zshalias
 ln -s $FILE_DIR/.common_export $HOME/.zshenv
 ln -s $FILE_DIR/.gitconfig $HOME/.gitconfig
 ln -s $FILE_DIR/.gitmessage $HOME/.gitmessage
-#ln -s $FILE_DIR/.vimrc $HOME/.vimrc
 ln -s $FILE_DIR/.tmux.conf $HOME/.tmux.conf
-# ln -s $HOME/.local/share/vimplug $HOME/.vim
 ln -s $HOME/.visualarts/.gnupg $HOME/.gnupg
 ln -s $HOME/.visualarts/.ssh $HOME/.ssh
 ln -s $FILE_DIR/tools/nvim $HOME/.config/nvim
-
